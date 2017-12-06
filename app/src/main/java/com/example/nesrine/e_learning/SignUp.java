a7dedb96dba70b21d39f498304addb8fc12142d1
Nesrine <projetmobile2018@gmail.com>
2017-12-06 15:03:52 +0100

    this juste login page and signup

--- app/src/main/java/com/example/nesrine/e_learning/SignUp.java
+++ app/src/main/java/com/example/nesrine/e_learning/SignUp.java
@@ -1,13 +1,30 @@
 package com.example.nesrine.e_learning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SignUp extends AppCompatActivity {
-
+    Button login, sign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        login=(Button)findViewById(R.id.loginB);
+        sign =(Button)findViewById(R.id.signupB);
+    }
+    //Methode navigation  Button sigup
+    public void gotosign(View view)
+    {
+        Intent intent = new Intent(this,SignUp.class);
+        startActivity(intent);
+    }
+    //Methode navigation  Button Login
+    public void gotologin(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
     }
 }
